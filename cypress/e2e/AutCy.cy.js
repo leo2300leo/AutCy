b948e8d703d3ae0dcd51cb91722e33ec59c47ccc
Leonardo Caetano <leonardo.caetano@efacec.com>
2024-06-19 18:15:55 +0100

    coming

--- cypress/e2e/AutCy.cy.js
+++ cypress/e2e/AutCy.cy.js
@@ -2,10 +2,8 @@ describe('template spec', () => {
   it('passes', () => {
     // we aren't logged in, so our web server
     // redirected us to /login
-      cy.visit("http://172.19.181.178");
-      cy.visit({
-        url: "http://172.19.181.178/login",
-        method: "GET",
+      cy.visit("http://172.19.181.178").$$('login')
+    
       });
   })
 })
