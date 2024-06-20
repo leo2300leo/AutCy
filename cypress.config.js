d2507b5c433fed8fbcc24572390aebb362eb1d8c
Leonardo Caetano <leonardo.caetano@efacec.com>
2024-06-20 15:42:33 +0100

    coming

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,25 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "eerz7u",
   e2e: {
+    screenshotOnRunFailure: true,
+    screenshotsFolder: "cypress/screenshots",
+    scrollBehavior: "top",
+    slowTestThreshold: 10000,
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.{js,jsx,ts,tsx}",
+    supportFolder: false,
+    taskTimeout: 60000,
+    testIsolation: true,
+    trashAssetsBeforeRuns: true,
+    userAgent: null,
+    video: false,
+    videoCompression: false,
+    videosFolder: "cypress/videos",
+    viewportHeight: 1000,
+    viewportWidth: 1000,
+    waitForAnimations: true,
+    watchForFileChanges: true,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/AutCy.cy.js
+++ cypress/e2e/AutCy.cy.js
@@ -1,9 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    // we aren't logged in, so our web server
-    // redirected us to /login
-      cy.visit("http://172.19.181.178").$$('login')
-    
-      });
-  })
-})
+describe("The Home Page", () => {
+  it("successfully loads", () => {
+    cy.visit("http://172.19.181.178"); // change URL to match your dev URL
+  });
+});
