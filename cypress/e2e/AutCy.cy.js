6c82cda35d058f196d8a001e400bc36a1f795b3e
Leonardo Caetano <leonardo.caetano@efacec.com>
2024-06-25 09:49:49 +0100

    coming

--- cypress/e2e/AutCy.cy.js
+++ cypress/e2e/AutCy.cy.js
@@ -1,5 +1,6 @@
 describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("172.19.181.178", {headers: { "Accept-Encoding": "gzip, deflate" },});// change URL to match your dev URL
+  
   });
 });
